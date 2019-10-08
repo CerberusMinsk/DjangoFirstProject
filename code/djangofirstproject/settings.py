8d52409a1120b7c639d2c91b4b6e4f6015e1bb78
cerberus.minsk <cerberus.minsk@gmail.com>
2019-10-08 18:34:35 +0300

    conect to DB inside docker

--- code/djangofirstproject/settings.py
+++ code/djangofirstproject/settings.py
@@ -81,6 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD': '123456',
         'HOST': 'db',
         'PORT': 5432,
     }
